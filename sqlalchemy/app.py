0d6336688febe0b0ece05d9be8788640d3d98cea
Vichea <48352653+vicheanath@users.noreply.github.com>
2024-04-07 17:46:45 -0500

    add : one-to-many-relationship

--- sqlalchemy/app.py
+++ sqlalchemy/app.py
@@ -1,14 +1,26 @@
 
-from sqlalchemy.orm import sessionmaker
 
-from models import User, engine
+from models import User, session, Address
 
-# Create a session
-Session = sessionmaker(bind=engine)
+# Create a user
+user = User(name="John", age=30)
+user2 = User(name="Jane", age=25)
 
-session = Session()
+# Create an address
+address = Address(street="123 Main St", city="New York", state="NY", zip="10001")
+address2 = Address(street="456 Elm St", city="Los Angeles", state="CA", zip="90001")
+address3 = Address(street="789 Oak St", city="Chicago", state="IL", zip="60007")
+# Add the address to the user
+user.addresses.extend([address, address2])
+user2.addresses.append(address3)
 
-users = session.query(User).all()
+# Add the user to the session
+session.add(user)
+session.add(user2)
+session.commit()
 
-for user in users:
-    print(user)
+print(f"User: {user.addresses = }")
+print(f"User: {user2.addresses = }")
+
+
+print(f"Address: {address.user = }")
--- sqlalchemy/models.py
+++ sqlalchemy/models.py
@@ -1,5 +1,11 @@
-from sqlalchemy import Column,String,Integer, create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, String, Integer, create_engine, ForeignKey
+from sqlalchemy.orm import (
+    declarative_base,
+    sessionmaker,
+    relationship,
+    Mapped,
+    mapped_column,
+)
 
 
 DB_URL = "sqlite:///database.db"
@@ -8,13 +14,38 @@ engine = create_engine(DB_URL)
 
 Base = declarative_base()
 
-class User(Base):
-    __tablename__ = "users"
-    
-    id = Column(Integer, primary_key=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+class BaseModel(Base):
+    __abstract__ = True
+    __allow_unmapped__ = True
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+
+class Address(BaseModel):
+    __tablename__ = "address"
+
+    street = Column(String)
+    city = Column(String)
+    state = Column(String)
+    zip = Column(String)
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    user: Mapped["User"] = relationship(back_populates="addresses")
+
+    def __repr__(self):
+        return f"<Address(street={self.street}, city={self.city}, state={self.state}, zip={self.zip})>"
+
+
+class User(BaseModel):
+    __tablename__ = "user"
+
     name = Column(String)
     age = Column(Integer)
-    
+    addresses: Mapped[list["Address"]] = relationship()
+
     def __repr__(self):
         return f"<User(name={self.name}, age={self.age})>"
 
